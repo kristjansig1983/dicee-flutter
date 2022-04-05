f7943bb60eb6046f23bb0542cf164949b90a8dc3
kristjansig1983 <91916422+kristjansig1983@users.noreply.github.com>
2022-04-05 17:45:40 +0000

    Centered dice
    Dice have been centered

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\ks10\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\ks10\StudioProjects\dicee-flutter"
+export "FLUTTER_ROOT=C:\Users\kriss\flutter\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\kriss\StudioProjects\dicee-flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
