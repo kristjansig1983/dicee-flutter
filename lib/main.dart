15802cc129a6a1c7ce19c2957ddc883bcd06ad39
kristjansig1983 <91916422+kristjansig1983@users.noreply.github.com>
2022-04-06 13:31:02 +0000

    Fullklárað
    Get kastað teningum eins og ekkert sé sjálfsagðara

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,24 +16,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    var leftDiceNumber = 5;
-    var rightDiceNumber = 2;
     return Center(
       child: Row(
         children: [
           Expanded(
             child: FlatButton(
-                onPressed: () {}, child: Image.asset('images/dice$leftDiceNumber.png')),
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
           ),
           Expanded(
             child: FlatButton(
-                onPressed: () {}, child: Image.asset('images/dice$rightDiceNumber.png')),
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
           ),
         ],
       ),
     );
   }
 }
+
